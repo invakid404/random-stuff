4f4c0e1d9a3d5bdadf4920b4847d8e66727eb899
invakid404 <emi.boneva@abv.bg>
2020-08-20 14:15:09 +0300

    Added "Sum of the deepest nodes" solution

---
+++ binarysearch/sum_of_deepest_nodes.cpp
@@ -0,0 +1,67 @@
+#include <bits/stdc++.h>
+
+#define ENDL '\n'
+
+namespace {
+class Tree {
+public:
+	int val;
+	Tree* left;
+	Tree* right;
+};
+
+#define SAFE_CALL(node, f)                                                    \
+	{                                                                         \
+		if (node != nullptr) {                                                \
+			f(node);                                                          \
+		}                                                                     \
+	}
+
+class Solution {
+public:
+	auto solve(Tree* root) {
+		if (root == nullptr) {
+			return 0;
+		}
+
+		int level_sum;
+
+		auto q = std::queue<Tree*>();
+		q.emplace(root);
+
+		while (!q.empty()) {
+			level_sum = 0;
+			auto level_amt = q.size();
+
+			while (level_amt--) {
+				auto curr_node = q.front();
+				q.pop();
+
+				SAFE_CALL(curr_node->left, q.emplace);
+				SAFE_CALL(curr_node->right, q.emplace);
+
+				level_sum += curr_node->val;
+			}
+		}
+
+		return level_sum;
+	}
+};
+} // namespace
+
+int main() {
+	std::ios_base::sync_with_stdio(false);
+	std::cin.tie(nullptr);
+	std::cout.tie(nullptr);
+
+	Tree* root = new Tree();
+	root->val = -5;
+
+	root->left = new Tree();
+	root->left->val = 90;
+
+	Solution solution;
+	std::cout << solution.solve(root) << ENDL;
+
+	return 0;
+}
