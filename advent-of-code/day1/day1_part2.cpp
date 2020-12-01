86d0fb9725ea1aa09063928c28a7fd5eed464e0d
invakid404 <splashgamingbg@gmail.com>
2020-12-01 12:23:18 +0200

    Oops

--- advent-of-code/day1/day1_part2.cpp
+++ advent-of-code/day1/day1_part2.cpp
@@ -15,18 +15,25 @@ int main() {
         vec.emplace_back(foo);
     }
 
+    auto res = -1;
     for (auto i = 0; i < vec.size(); ++i) {
         std::unordered_set<int> complements;
         for (auto j = i + 1; j < vec.size(); ++j) {
             auto complement = 2020 - vec[i] - vec[j];
             if (complements.find(complement) != complements.end()) {
-                std::cout << vec[i] * vec[j] * complement << ENDL;
+                res = vec[i] * vec[j] * complement;
                 break;
             }
 
             complements.insert(vec[j]);
         }
+
+        if (res != -1) {
+            break;
+        }
     }
+
+    std::cout << res << ENDL;
 
     return 0;
 }
