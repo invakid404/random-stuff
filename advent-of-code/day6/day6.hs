d81625b6191b0b8f5ecdb3e3e43276c14b8f8c0f
invakid404 <emi.boneva@abv.bg>
2020-12-06 12:46:56 +0200

    Added day 6 solution

---
+++ advent-of-code/day6/day6.hs
@@ -0,0 +1,18 @@
+import Control.Monad (liftM2)
+import Data.Bits (Bits (bit, popCount, (.&.), (.|.)))
+import Data.Char (ord)
+import Data.Foldable (Foldable (foldl'))
+import Data.List.Split (splitOn)
+
+main :: IO ()
+main =
+  liftM2
+    (>>)
+    (print . solve (.|.))
+    (print . solve (.&.))
+    . map (map (foldl' ((. (bit . subtract (ord 'a') . ord)) . (.|.)) (0 :: Int)))
+    . splitOn [""]
+    . lines
+    =<< readFile "input.txt"
+  where
+    solve = (sum .) . map . (popCount .) . foldl1
