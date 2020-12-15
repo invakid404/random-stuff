6820987fdf55ec1eb1ef0d703a6d09018a150af3
invakid404 <splashgamingbg@gmail.com>
2020-12-15 09:38:12 +0200

    Added day 15 solution

---
+++ advent-of-code/day15/day15.hs
@@ -0,0 +1,34 @@
+import Control.Monad (foldM, forM_, liftM2)
+import Control.Monad.ST (ST, runST)
+import Data.Array.ST
+  ( MArray (newArray),
+    STUArray,
+    readArray,
+    writeArray,
+  )
+import Data.List.Split (splitOn)
+
+if' :: Bool -> p -> p -> p
+if' True x _ = x
+if' False _ y = y
+
+run :: Int -> [Int] -> Int
+run n inp = runST $ do
+  arr <- newArray (0, n) (-1) :: ST s (STUArray s Int Int)
+  forM_ (zip inp [1 ..]) $ uncurry (writeArray arr)
+
+  flip (`foldM` 0) [length inp + 1 .. n - 1] $ \v i -> do
+    val <- readArray arr v
+    writeArray arr v i
+
+    pure $ if' (val == -1) 0 (i - val)
+
+main :: IO ()
+main =
+  liftM2
+    (>>)
+    (print . run 2020)
+    (print . run 30000000)
+    . map read
+    . splitOn ","
+    =<< readFile "input.txt"
