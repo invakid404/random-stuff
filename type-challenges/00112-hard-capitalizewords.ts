dead6a28edd23497fa07a277c3faf0803fcd3590
invakid404 <invakid404@riseup.net>
2022-08-19 17:18:50 +0300

    capitalize words solution

---
+++ type-challenges/00112-hard-capitalizewords.ts
@@ -0,0 +1,71 @@
+/*
+  112 - Capitalize Words
+  -------
+  by Anthony Fu (@antfu) #hard #template-literal
+  
+  ### Question
+  
+  Implement `CapitalizeWords<T>` which converts the first letter of **each word of a string** to uppercase and leaves the rest as-is.
+  
+  For example
+  
+  ```ts
+  type capitalized = CapitalizeWords<'hello world, my friends'> // expected to be 'Hello World, My Friends'
+  ```
+  
+  > View on GitHub: https://tsch.js.org/112
+*/
+
+/* _____________ Your Code Here _____________ */
+
+type Split<Value extends string, Delimiter extends string> = Value extends ''
+  ? []
+  : Value extends `${infer Prefix}${Delimiter}${infer Suffix}`
+  ? [Prefix, ...Split<Suffix, Delimiter>]
+  : [Value];
+
+type Characters<Value extends string> = Split<Value, ''>;
+
+type LowercaseLetters = 'abcdefghijklmnopqrstuvwxyz';
+type UppercaseLetters = Uppercase<LowercaseLetters>;
+
+type Letter = [
+  ...Characters<LowercaseLetters>,
+  ...Characters<UppercaseLetters>,
+][number];
+
+type CapitalizeWordsRec<S extends string> =
+  S extends `${infer First}${infer Rest}`
+    ? CapitalizeWordsRec<Rest> extends infer Result extends string
+      ? First extends Letter
+        ? `${First}${Result}`
+        : `${First}${Capitalize<Result>}`
+      : never
+    : S;
+
+type CapitalizeWords<S extends string> = Capitalize<CapitalizeWordsRec<S>>;
+
+/* _____________ Test Cases _____________ */
+import type { Equal, Expect } from '@type-challenges/utils';
+
+type cases = [
+  Expect<Equal<CapitalizeWords<'foobar'>, 'Foobar'>>,
+  Expect<Equal<CapitalizeWords<'FOOBAR'>, 'FOOBAR'>>,
+  Expect<Equal<CapitalizeWords<'foo bar'>, 'Foo Bar'>>,
+  Expect<Equal<CapitalizeWords<'foo bar hello world'>, 'Foo Bar Hello World'>>,
+  Expect<Equal<CapitalizeWords<'foo bar.hello,world'>, 'Foo Bar.Hello,World'>>,
+  Expect<
+    Equal<
+      CapitalizeWords<'aa!bb@cc#dd$ee%ff^gg&hh*ii(jj)kk_ll+mm{nn}oo|pp🤣qq'>,
+      'Aa!Bb@Cc#Dd$Ee%Ff^Gg&Hh*Ii(Jj)Kk_Ll+Mm{Nn}Oo|Pp🤣Qq'
+    >
+  >,
+  Expect<Equal<CapitalizeWords<''>, ''>>,
+];
+
+/* _____________ Further Steps _____________ */
+/*
+  > Share your solutions: https://tsch.js.org/112/answer
+  > View solutions: https://tsch.js.org/112/solutions
+  > More Challenges: https://tsch.js.org
+*/
