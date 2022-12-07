deadbb508974678e09e18c0003b0790936b7d28e
invakid404 <invakid404@riseup.net>
2022-12-07 15:02:24 +0200

    day 7 solution

---
+++ advent-of-code/2022/day7.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+folder_sizes = Hash.new(0)
+
+File.foreach('day7.txt').map(&:split).each_with_object([]) do |line, stack|
+  case line
+  in ['$', 'cd', '..']
+    stack.pop
+  in ['$', 'cd', folder]
+    stack.push [stack.last, folder].compact.join('/')
+  in [size, _] if size.match?(/^\d+$/)
+    stack.each { |folder| folder_sizes[folder] += size.to_i }
+  else
+  end
+end
+
+puts folder_sizes.values.select { |value| value <= 100_000 }.sum
+puts folder_sizes.values.select { |value| value >= folder_sizes['/'] - 40_000_000 }.min
