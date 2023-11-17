dead96a2d4f06026a888aa0d6638c1ab4a410cca
invakid404 <invakid404@riseup.net>
2023-11-17 23:17:21 +0200

    to primitive solution

---
+++ type-challenges/16259-medium-to-primitive.ts
@@ -0,0 +1,85 @@
+/*
+  16259 - ToPrimitive
+  -------
+  by 前端子鱼 (@mwc) #medium
+
+  ### Question
+
+  Convert a property of type literal (label type) to a primitive type.
+
+  For example
+
+  ```typescript
+  type X = {
+    name: 'Tom',
+    age: 30,
+    married: false,
+    addr: {
+      home: '123456',
+      phone: '13111111111'
+    }
+  }
+
+  type Expected = {
+    name: string,
+    age: number,
+    married: boolean,
+    addr: {
+      home: string,
+      phone: string
+    }
+  }
+  type Todo = ToPrimitive<X> // should be same as `Expected`
+  ```
+
+  > View on GitHub: https://tsch.js.org/16259
+*/
+
+/* _____________ Your Code Here _____________ */
+type ToPrimitive<T> = T extends object
+  ? {
+      [Key in keyof T]: T[Key] extends Function
+        ? Function
+        : ToPrimitive<T[Key]>;
+    }
+  : T extends { valueOf: () => infer P }
+  ? P
+  : never;
+
+/* _____________ Test Cases _____________ */
+import type { Equal, Expect } from '@type-challenges/utils';
+
+type PersonInfo = {
+  name: 'Tom';
+  age: 30;
+  married: false;
+  addr: {
+    home: '123456';
+    phone: '13111111111';
+  };
+  hobbies: ['sing', 'dance'];
+  readonlyArr: readonly ['test'];
+  fn: () => any;
+};
+
+type ExpectedResult = {
+  name: string;
+  age: number;
+  married: boolean;
+  addr: {
+    home: string;
+    phone: string;
+  };
+  hobbies: [string, string];
+  readonlyArr: readonly [string];
+  fn: Function;
+};
+
+type cases = [Expect<Equal<ToPrimitive<PersonInfo>, ExpectedResult>>];
+
+/* _____________ Further Steps _____________ */
+/*
+  > Share your solutions: https://tsch.js.org/16259/answer
+  > View solutions: https://tsch.js.org/16259/solutions
+  > More Challenges: https://tsch.js.org
+*/
