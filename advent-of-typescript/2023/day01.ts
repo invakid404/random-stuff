deadeb0028f2443f105022a279c828bce6c28137
invakid404 <invakid404@riseup.net>
2023-12-01 13:46:06 +0200

    aot day 1

---
+++ advent-of-typescript/2023/day01.ts
@@ -0,0 +1,8 @@
+type SantasFavoriteCookies = "ginger-bread" | "chocolate-chip";
+
+import { Expect, Equal } from "type-testing";
+
+type test_0_actual = SantasFavoriteCookies;
+//   ^?
+type test_0_expected = "ginger-bread" | "chocolate-chip";
+type test_0 = Expect<Equal<test_0_actual, test_0_expected>>;
