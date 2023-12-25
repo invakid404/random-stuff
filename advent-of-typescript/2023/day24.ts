deade24eb499840afafea440271f13810d621e26
invakid404 <invakid404@riseup.net>
2023-12-25 23:27:20 +0200

    aot day 24

---
+++ advent-of-typescript/2023/day24.ts
@@ -0,0 +1,376 @@
+type Alley = "  ";
+type Santa = "🎅";
+type MazeItem = "🎄" | Santa | Alley;
+type DELICIOUS_COOKIES = "🍪";
+type MazeMatrix = MazeItem[][];
+type Directions = "up" | "down" | "left" | "right";
+
+type Move<
+  T extends MazeMatrix,
+  Direction extends Directions,
+> = FindSanta<T> extends [
+  infer SantaRow extends readonly unknown[],
+  infer SantaCol extends readonly unknown[],
+]
+  ? Next<SantaRow, SantaCol, Direction> extends [
+      infer NextRow extends readonly unknown[],
+      infer NextCol extends readonly unknown[],
+    ]
+    ? InBounds<T, NextRow, NextCol> extends false
+      ? Win<T>
+      : CanMove<T, NextRow, NextCol> extends true
+        ? SetAt<SetAt<T, Alley, SantaRow, SantaCol>, Santa, NextRow, NextCol>
+        : T
+    : Win<T>
+  : never;
+
+type FindSanta<
+  T extends MazeMatrix,
+  Row extends readonly unknown[] = [],
+> = T extends [
+  infer Head extends MazeMatrix[number],
+  ...infer Rest extends MazeMatrix,
+]
+  ? FindSantaInRow<Head> extends infer Col extends readonly unknown[]
+    ? [Col] extends [never]
+      ? FindSanta<Rest, [...Row, unknown]>
+      : [Row, Col]
+    : never
+  : never;
+
+type FindSantaInRow<
+  T extends MazeMatrix[number],
+  Col extends readonly unknown[] = [],
+> = T extends [infer Head, ...infer Rest extends MazeMatrix[number]]
+  ? Head extends Santa
+    ? Col
+    : FindSantaInRow<Rest, [...Col, unknown]>
+  : never;
+
+type Next<
+  Row extends readonly unknown[],
+  Col extends readonly unknown[],
+  Direction extends Directions,
+> = Direction extends "up"
+  ? [Tail<Row>, Col]
+  : Direction extends "down"
+    ? [[...Row, unknown], Col]
+    : Direction extends "left"
+      ? [Row, Tail<Col>]
+      : [Row, [...Col, unknown]];
+
+type Tail<T extends readonly unknown[]> = T extends [unknown, ...infer Rest]
+  ? Rest
+  : unknown;
+
+type InBounds<
+  T extends MazeMatrix,
+  Row extends readonly unknown[],
+  Col extends readonly unknown[],
+> = Row["length"] extends keyof T
+  ? Col["length"] extends keyof T[Row["length"]]
+    ? true
+    : false
+  : false;
+
+type CanMove<
+  T extends MazeMatrix,
+  Row extends readonly unknown[],
+  Col extends readonly unknown[],
+> = T[Row["length"]][Col["length"]] extends Alley ? true : false;
+
+type SetAt<
+  T extends MazeMatrix,
+  Value extends MazeItem,
+  Row extends readonly unknown[],
+  Col extends readonly unknown[],
+> = {
+  [R in keyof T]: R extends `${Row["length"]}`
+    ? SetInRow<T[R], Value, Col>
+    : T[R];
+};
+
+type SetInRow<
+  T extends MazeMatrix[number],
+  Value extends MazeItem,
+  Col extends readonly unknown[],
+> = {
+  [C in keyof T]: C extends `${Col["length"]}` ? Value : T[C];
+};
+
+type Win<T extends MazeMatrix> = {
+  [Row in keyof T]: WinRow<T[Row]>;
+};
+
+type WinRow<T extends MazeMatrix[number]> = {
+  [Col in keyof T]: DELICIOUS_COOKIES;
+};
+
+import { Expect, Equal } from "type-testing";
+
+type Maze0 = [
+  ["🎄", "🎄", "🎄", "🎄", "🎄", "🎄", "🎄", "🎄", "🎄", "🎄"],
+  ["🎄", "🎄", "🎄", "🎄", "🎄", "🎄", "🎅", "🎄", "🎄", "🎄"],
+  ["🎄", "🎄", "🎄", "🎄", "  ", "🎄", "  ", "  ", "  ", "🎄"],
+  ["🎄", "🎄", "🎄", "🎄", "  ", "🎄", "  ", "🎄", "  ", "🎄"],
+  ["🎄", "  ", "  ", "  ", "  ", "🎄", "  ", "🎄", "  ", "🎄"],
+  ["  ", "  ", "🎄", "🎄", "  ", "  ", "  ", "🎄", "🎄", "🎄"],
+  ["🎄", "  ", "🎄", "🎄", "  ", "🎄", "  ", "🎄", "🎄", "🎄"],
+  ["🎄", "  ", "🎄", "🎄", "  ", "🎄", "  ", "🎄", "🎄", "🎄"],
+  ["🎄", "  ", "  ", "  ", "  ", "🎄", "  ", "🎄", "🎄", "🎄"],
+  ["🎄", "🎄", "🎄", "🎄", "🎄", "🎄", "🎄", "🎄", "🎄", "🎄"],
+];
+
+// can't move up!
+type test_maze0_up_actual = Move<Maze0, "up">;
+//   ^?
+type test_maze0_up = Expect<Equal<test_maze0_up_actual, Maze0>>;
+
+// but Santa can move down!
+type test_maze0_down_actual = Move<Maze0, "down">;
+//   ^?
+type Maze1 = [
+  ["🎄", "🎄", "🎄", "🎄", "🎄", "🎄", "🎄", "🎄", "🎄", "🎄"],
+  ["🎄", "🎄", "🎄", "🎄", "🎄", "🎄", "  ", "🎄", "🎄", "🎄"],
+  ["🎄", "🎄", "🎄", "🎄", "  ", "🎄", "🎅", "  ", "  ", "🎄"],
+  ["🎄", "🎄", "🎄", "🎄", "  ", "🎄", "  ", "🎄", "  ", "🎄"],
+  ["🎄", "  ", "  ", "  ", "  ", "🎄", "  ", "🎄", "  ", "🎄"],
+  ["  ", "  ", "🎄", "🎄", "  ", "  ", "  ", "🎄", "🎄", "🎄"],
+  ["🎄", "  ", "🎄", "🎄", "  ", "🎄", "  ", "🎄", "🎄", "🎄"],
+  ["🎄", "  ", "🎄", "🎄", "  ", "🎄", "  ", "🎄", "🎄", "🎄"],
+  ["🎄", "  ", "  ", "  ", "  ", "🎄", "  ", "🎄", "🎄", "🎄"],
+  ["🎄", "🎄", "🎄", "🎄", "🎄", "🎄", "🎄", "🎄", "🎄", "🎄"],
+];
+type test_maze0_down = Expect<Equal<test_maze0_down_actual, Maze1>>;
+
+// Santa can move down again!
+type test_maze1_down_actual = Move<Maze1, "down">;
+type Maze2 = [
+  ["🎄", "🎄", "🎄", "🎄", "🎄", "🎄", "🎄", "🎄", "🎄", "🎄"],
+  ["🎄", "🎄", "🎄", "🎄", "🎄", "🎄", "  ", "🎄", "🎄", "🎄"],
+  ["🎄", "🎄", "🎄", "🎄", "  ", "🎄", "  ", "  ", "  ", "🎄"],
+  ["🎄", "🎄", "🎄", "🎄", "  ", "🎄", "🎅", "🎄", "  ", "🎄"],
+  ["🎄", "  ", "  ", "  ", "  ", "🎄", "  ", "🎄", "  ", "🎄"],
+  ["  ", "  ", "🎄", "🎄", "  ", "  ", "  ", "🎄", "🎄", "🎄"],
+  ["🎄", "  ", "🎄", "🎄", "  ", "🎄", "  ", "🎄", "🎄", "🎄"],
+  ["🎄", "  ", "🎄", "🎄", "  ", "🎄", "  ", "🎄", "🎄", "🎄"],
+  ["🎄", "  ", "  ", "  ", "  ", "🎄", "  ", "🎄", "🎄", "🎄"],
+  ["🎄", "🎄", "🎄", "🎄", "🎄", "🎄", "🎄", "🎄", "🎄", "🎄"],
+];
+type test_maze1_down = Expect<Equal<test_maze1_down_actual, Maze2>>;
+
+// can't move left!
+type test_maze2_left_actual = Move<Maze2, "left">;
+//   ^?
+type test_maze2_left = Expect<Equal<test_maze2_left_actual, Maze2>>;
+
+// if Santa moves up, it's back to Maze1!
+type test_maze2_up_actual = Move<Maze2, "up">;
+//   ^?
+type test_maze2_up = Expect<Equal<test_maze2_up_actual, Maze1>>;
+
+// can't move right!
+type test_maze2_right_actual = Move<Maze2, "right">;
+//   ^?
+type test_maze2_right = Expect<Equal<test_maze2_right_actual, Maze2>>;
+
+// we exhausted all other options! guess we gotta go down!
+type test_maze2_down_actual = Move<Maze2, "down">;
+//   ^?
+type Maze3 = [
+  ["🎄", "🎄", "🎄", "🎄", "🎄", "🎄", "🎄", "🎄", "🎄", "🎄"],
+  ["🎄", "🎄", "🎄", "🎄", "🎄", "🎄", "  ", "🎄", "🎄", "🎄"],
+  ["🎄", "🎄", "🎄", "🎄", "  ", "🎄", "  ", "  ", "  ", "🎄"],
+  ["🎄", "🎄", "🎄", "🎄", "  ", "🎄", "  ", "🎄", "  ", "🎄"],
+  ["🎄", "  ", "  ", "  ", "  ", "🎄", "🎅", "🎄", "  ", "🎄"],
+  ["  ", "  ", "🎄", "🎄", "  ", "  ", "  ", "🎄", "🎄", "🎄"],
+  ["🎄", "  ", "🎄", "🎄", "  ", "🎄", "  ", "🎄", "🎄", "🎄"],
+  ["🎄", "  ", "🎄", "🎄", "  ", "🎄", "  ", "🎄", "🎄", "🎄"],
+  ["🎄", "  ", "  ", "  ", "  ", "🎄", "  ", "🎄", "🎄", "🎄"],
+  ["🎄", "🎄", "🎄", "🎄", "🎄", "🎄", "🎄", "🎄", "🎄", "🎄"],
+];
+type test_maze2_down = Expect<Equal<test_maze2_down_actual, Maze3>>;
+
+// maybe we just gotta go down all the time?
+type test_maze3_down_actual = Move<Maze3, "down">;
+//   ^?
+type Maze4 = [
+  ["🎄", "🎄", "🎄", "🎄", "🎄", "🎄", "🎄", "🎄", "🎄", "🎄"],
+  ["🎄", "🎄", "🎄", "🎄", "🎄", "🎄", "  ", "🎄", "🎄", "🎄"],
+  ["🎄", "🎄", "🎄", "🎄", "  ", "🎄", "  ", "  ", "  ", "🎄"],
+  ["🎄", "🎄", "🎄", "🎄", "  ", "🎄", "  ", "🎄", "  ", "🎄"],
+  ["🎄", "  ", "  ", "  ", "  ", "🎄", "  ", "🎄", "  ", "🎄"],
+  ["  ", "  ", "🎄", "🎄", "  ", "  ", "🎅", "🎄", "🎄", "🎄"],
+  ["🎄", "  ", "🎄", "🎄", "  ", "🎄", "  ", "🎄", "🎄", "🎄"],
+  ["🎄", "  ", "🎄", "🎄", "  ", "🎄", "  ", "🎄", "🎄", "🎄"],
+  ["🎄", "  ", "  ", "  ", "  ", "🎄", "  ", "🎄", "🎄", "🎄"],
+  ["🎄", "🎄", "🎄", "🎄", "🎄", "🎄", "🎄", "🎄", "🎄", "🎄"],
+];
+type test_maze3_down = Expect<Equal<test_maze3_down_actual, Maze4>>;
+
+// let's go left this time just to change it up?
+type test_maze4_left_actual = Move<Maze4, "left">;
+//   ^?
+type Maze5 = [
+  ["🎄", "🎄", "🎄", "🎄", "🎄", "🎄", "🎄", "🎄", "🎄", "🎄"],
+  ["🎄", "🎄", "🎄", "🎄", "🎄", "🎄", "  ", "🎄", "🎄", "🎄"],
+  ["🎄", "🎄", "🎄", "🎄", "  ", "🎄", "  ", "  ", "  ", "🎄"],
+  ["🎄", "🎄", "🎄", "🎄", "  ", "🎄", "  ", "🎄", "  ", "🎄"],
+  ["🎄", "  ", "  ", "  ", "  ", "🎄", "  ", "🎄", "  ", "🎄"],
+  ["  ", "  ", "🎄", "🎄", "  ", "🎅", "  ", "🎄", "🎄", "🎄"],
+  ["🎄", "  ", "🎄", "🎄", "  ", "🎄", "  ", "🎄", "🎄", "🎄"],
+  ["🎄", "  ", "🎄", "🎄", "  ", "🎄", "  ", "🎄", "🎄", "🎄"],
+  ["🎄", "  ", "  ", "  ", "  ", "🎄", "  ", "🎄", "🎄", "🎄"],
+  ["🎄", "🎄", "🎄", "🎄", "🎄", "🎄", "🎄", "🎄", "🎄", "🎄"],
+];
+// it worked!
+type test_maze4_left = Expect<Equal<test_maze4_left_actual, Maze5>>;
+
+// couldn't hurt to try left again?
+type test_maze5_left_actual = Move<Maze5, "left">;
+//   ^?
+type Maze6 = [
+  ["🎄", "🎄", "🎄", "🎄", "🎄", "🎄", "🎄", "🎄", "🎄", "🎄"],
+  ["🎄", "🎄", "🎄", "🎄", "🎄", "🎄", "  ", "🎄", "🎄", "🎄"],
+  ["🎄", "🎄", "🎄", "🎄", "  ", "🎄", "  ", "  ", "  ", "🎄"],
+  ["🎄", "🎄", "🎄", "🎄", "  ", "🎄", "  ", "🎄", "  ", "🎄"],
+  ["🎄", "  ", "  ", "  ", "  ", "🎄", "  ", "🎄", "  ", "🎄"],
+  ["  ", "  ", "🎄", "🎄", "🎅", "  ", "  ", "🎄", "🎄", "🎄"],
+  ["🎄", "  ", "🎄", "🎄", "  ", "🎄", "  ", "🎄", "🎄", "🎄"],
+  ["🎄", "  ", "🎄", "🎄", "  ", "🎄", "  ", "🎄", "🎄", "🎄"],
+  ["🎄", "  ", "  ", "  ", "  ", "🎄", "  ", "🎄", "🎄", "🎄"],
+  ["🎄", "🎄", "🎄", "🎄", "🎄", "🎄", "🎄", "🎄", "🎄", "🎄"],
+];
+type test_maze5_left = Expect<Equal<test_maze5_left_actual, Maze6>>;
+
+// three time's a charm?
+type test_maze6_left_actual = Move<Maze6, "left">;
+//   ^?
+// lol, nope.
+type test_maze6_left = Expect<Equal<test_maze6_left_actual, Maze6>>;
+
+// we haven't tried up yet (?)
+type test_maze6_up_actual = Move<Maze6, "up">;
+//   ^?
+type Maze7 = [
+  ["🎄", "🎄", "🎄", "🎄", "🎄", "🎄", "🎄", "🎄", "🎄", "🎄"],
+  ["🎄", "🎄", "🎄", "🎄", "🎄", "🎄", "  ", "🎄", "🎄", "🎄"],
+  ["🎄", "🎄", "🎄", "🎄", "  ", "🎄", "  ", "  ", "  ", "🎄"],
+  ["🎄", "🎄", "🎄", "🎄", "  ", "🎄", "  ", "🎄", "  ", "🎄"],
+  ["🎄", "  ", "  ", "  ", "🎅", "🎄", "  ", "🎄", "  ", "🎄"],
+  ["  ", "  ", "🎄", "🎄", "  ", "  ", "  ", "🎄", "🎄", "🎄"],
+  ["🎄", "  ", "🎄", "🎄", "  ", "🎄", "  ", "🎄", "🎄", "🎄"],
+  ["🎄", "  ", "🎄", "🎄", "  ", "🎄", "  ", "🎄", "🎄", "🎄"],
+  ["🎄", "  ", "  ", "  ", "  ", "🎄", "  ", "🎄", "🎄", "🎄"],
+  ["🎄", "🎄", "🎄", "🎄", "🎄", "🎄", "🎄", "🎄", "🎄", "🎄"],
+];
+// NOICE.
+type test_maze6_up = Expect<Equal<test_maze6_up_actual, Maze7>>;
+
+// maybe another left??
+type test_maze7_left_actual = Move<Maze7, "left">;
+//   ^?
+type Maze8 = [
+  ["🎄", "🎄", "🎄", "🎄", "🎄", "🎄", "🎄", "🎄", "🎄", "🎄"],
+  ["🎄", "🎄", "🎄", "🎄", "🎄", "🎄", "  ", "🎄", "🎄", "🎄"],
+  ["🎄", "🎄", "🎄", "🎄", "  ", "🎄", "  ", "  ", "  ", "🎄"],
+  ["🎄", "🎄", "🎄", "🎄", "  ", "🎄", "  ", "🎄", "  ", "🎄"],
+  ["🎄", "  ", "  ", "🎅", "  ", "🎄", "  ", "🎄", "  ", "🎄"],
+  ["  ", "  ", "🎄", "🎄", "  ", "  ", "  ", "🎄", "🎄", "🎄"],
+  ["🎄", "  ", "🎄", "🎄", "  ", "🎄", "  ", "🎄", "🎄", "🎄"],
+  ["🎄", "  ", "🎄", "🎄", "  ", "🎄", "  ", "🎄", "🎄", "🎄"],
+  ["🎄", "  ", "  ", "  ", "  ", "🎄", "  ", "🎄", "🎄", "🎄"],
+  ["🎄", "🎄", "🎄", "🎄", "🎄", "🎄", "🎄", "🎄", "🎄", "🎄"],
+];
+type test_maze7_left = Expect<Equal<test_maze7_left_actual, Maze8>>;
+
+// haven't tried a right yet.. let's give it a go!
+type test_maze7_right_actual = Move<Maze8, "right">;
+//   ^?
+// not this time...
+type test_maze7_right = Expect<Equal<test_maze7_right_actual, Maze7>>;
+
+// probably just need to stick with left then
+type test_maze8_left_actual = Move<Maze8, "left">;
+//   ^?
+type Maze9 = [
+  ["🎄", "🎄", "🎄", "🎄", "🎄", "🎄", "🎄", "🎄", "🎄", "🎄"],
+  ["🎄", "🎄", "🎄", "🎄", "🎄", "🎄", "  ", "🎄", "🎄", "🎄"],
+  ["🎄", "🎄", "🎄", "🎄", "  ", "🎄", "  ", "  ", "  ", "🎄"],
+  ["🎄", "🎄", "🎄", "🎄", "  ", "🎄", "  ", "🎄", "  ", "🎄"],
+  ["🎄", "  ", "🎅", "  ", "  ", "🎄", "  ", "🎄", "  ", "🎄"],
+  ["  ", "  ", "🎄", "🎄", "  ", "  ", "  ", "🎄", "🎄", "🎄"],
+  ["🎄", "  ", "🎄", "🎄", "  ", "🎄", "  ", "🎄", "🎄", "🎄"],
+  ["🎄", "  ", "🎄", "🎄", "  ", "🎄", "  ", "🎄", "🎄", "🎄"],
+  ["🎄", "  ", "  ", "  ", "  ", "🎄", "  ", "🎄", "🎄", "🎄"],
+  ["🎄", "🎄", "🎄", "🎄", "🎄", "🎄", "🎄", "🎄", "🎄", "🎄"],
+];
+type test_maze8_left = Expect<Equal<test_maze8_left_actual, Maze9>>;
+
+// why fix what's not broken?
+type test_maze9_left_actual = Move<Maze9, "left">;
+//   ^?
+type Maze10 = [
+  ["🎄", "🎄", "🎄", "🎄", "🎄", "🎄", "🎄", "🎄", "🎄", "🎄"],
+  ["🎄", "🎄", "🎄", "🎄", "🎄", "🎄", "  ", "🎄", "🎄", "🎄"],
+  ["🎄", "🎄", "🎄", "🎄", "  ", "🎄", "  ", "  ", "  ", "🎄"],
+  ["🎄", "🎄", "🎄", "🎄", "  ", "🎄", "  ", "🎄", "  ", "🎄"],
+  ["🎄", "🎅", "  ", "  ", "  ", "🎄", "  ", "🎄", "  ", "🎄"],
+  ["  ", "  ", "🎄", "🎄", "  ", "  ", "  ", "🎄", "🎄", "🎄"],
+  ["🎄", "  ", "🎄", "🎄", "  ", "🎄", "  ", "🎄", "🎄", "🎄"],
+  ["🎄", "  ", "🎄", "🎄", "  ", "🎄", "  ", "🎄", "🎄", "🎄"],
+  ["🎄", "  ", "  ", "  ", "  ", "🎄", "  ", "🎄", "🎄", "🎄"],
+  ["🎄", "🎄", "🎄", "🎄", "🎄", "🎄", "🎄", "🎄", "🎄", "🎄"],
+];
+type test_maze9_left = Expect<Equal<test_maze9_left_actual, Maze10>>;
+
+// do you smell cookies?? it's coming from down..
+type test_maze10_down_actual = Move<Maze10, "down">;
+//   ^?
+type Maze11 = [
+  ["🎄", "🎄", "🎄", "🎄", "🎄", "🎄", "🎄", "🎄", "🎄", "🎄"],
+  ["🎄", "🎄", "🎄", "🎄", "🎄", "🎄", "  ", "🎄", "🎄", "🎄"],
+  ["🎄", "🎄", "🎄", "🎄", "  ", "🎄", "  ", "  ", "  ", "🎄"],
+  ["🎄", "🎄", "🎄", "🎄", "  ", "🎄", "  ", "🎄", "  ", "🎄"],
+  ["🎄", "  ", "  ", "  ", "  ", "🎄", "  ", "🎄", "  ", "🎄"],
+  ["  ", "🎅", "🎄", "🎄", "  ", "  ", "  ", "🎄", "🎄", "🎄"],
+  ["🎄", "  ", "🎄", "🎄", "  ", "🎄", "  ", "🎄", "🎄", "🎄"],
+  ["🎄", "  ", "🎄", "🎄", "  ", "🎄", "  ", "🎄", "🎄", "🎄"],
+  ["🎄", "  ", "  ", "  ", "  ", "🎄", "  ", "🎄", "🎄", "🎄"],
+  ["🎄", "🎄", "🎄", "🎄", "🎄", "🎄", "🎄", "🎄", "🎄", "🎄"],
+];
+type test_maze10_down = Expect<Equal<test_maze10_down_actual, Maze11>>;
+
+// the cookies must be freshly baked.  I hope there's also the customary glass of milk!
+type test_maze11_left_actual = Move<Maze11, "left">;
+//   ^?
+type Maze12 = [
+  ["🎄", "🎄", "🎄", "🎄", "🎄", "🎄", "🎄", "🎄", "🎄", "🎄"],
+  ["🎄", "🎄", "🎄", "🎄", "🎄", "🎄", "  ", "🎄", "🎄", "🎄"],
+  ["🎄", "🎄", "🎄", "🎄", "  ", "🎄", "  ", "  ", "  ", "🎄"],
+  ["🎄", "🎄", "🎄", "🎄", "  ", "🎄", "  ", "🎄", "  ", "🎄"],
+  ["🎄", "  ", "  ", "  ", "  ", "🎄", "  ", "🎄", "  ", "🎄"],
+  ["🎅", "  ", "🎄", "🎄", "  ", "  ", "  ", "🎄", "🎄", "🎄"],
+  ["🎄", "  ", "🎄", "🎄", "  ", "🎄", "  ", "🎄", "🎄", "🎄"],
+  ["🎄", "  ", "🎄", "🎄", "  ", "🎄", "  ", "🎄", "🎄", "🎄"],
+  ["🎄", "  ", "  ", "  ", "  ", "🎄", "  ", "🎄", "🎄", "🎄"],
+  ["🎄", "🎄", "🎄", "🎄", "🎄", "🎄", "🎄", "🎄", "🎄", "🎄"],
+];
+type test_maze11_left = Expect<Equal<test_maze11_left_actual, Maze12>>;
+
+// COOKIES!!!!!
+type test_maze12_left_actual = Move<Maze12, "left">;
+//   ^?
+type MazeWin = [
+  ["🍪", "🍪", "🍪", "🍪", "🍪", "🍪", "🍪", "🍪", "🍪", "🍪"],
+  ["🍪", "🍪", "🍪", "🍪", "🍪", "🍪", "🍪", "🍪", "🍪", "🍪"],
+  ["🍪", "🍪", "🍪", "🍪", "🍪", "🍪", "🍪", "🍪", "🍪", "🍪"],
+  ["🍪", "🍪", "🍪", "🍪", "🍪", "🍪", "🍪", "🍪", "🍪", "🍪"],
+  ["🍪", "🍪", "🍪", "🍪", "🍪", "🍪", "🍪", "🍪", "🍪", "🍪"],
+  ["🍪", "🍪", "🍪", "🍪", "🍪", "🍪", "🍪", "🍪", "🍪", "🍪"],
+  ["🍪", "🍪", "🍪", "🍪", "🍪", "🍪", "🍪", "🍪", "🍪", "🍪"],
+  ["🍪", "🍪", "🍪", "🍪", "🍪", "🍪", "🍪", "🍪", "🍪", "🍪"],
+  ["🍪", "🍪", "🍪", "🍪", "🍪", "🍪", "🍪", "🍪", "🍪", "🍪"],
+  ["🍪", "🍪", "🍪", "🍪", "🍪", "🍪", "🍪", "🍪", "🍪", "🍪"],
+];
+type test_maze12_left = Expect<Equal<test_maze12_left_actual, MazeWin>>;
